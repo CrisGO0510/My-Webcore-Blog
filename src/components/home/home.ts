7c4b3b5f071b3cbf1ce03610a53352ae3f73637d
CrisGO <cristhiangiraldo053@gmail.com>
2026-02-16 19:56:56 -0500

    header debe ir en app

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,11 +1,7 @@
 import { defineComponent } from "vue";
-import Header from "../header/header.vue";
 
 export default defineComponent({
   name: "Home",
-  components: {
-    Header,
-  },
   setup() {
     console.log("Home Component Loaded...");
     return {};
