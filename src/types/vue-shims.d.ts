9b62c02cc0a6149b2628d325f0466d8447fcae12
CrisGO <cristhiangiraldo053@gmail.com>
2026-03-30 18:25:06 -0500

    Correcciones a workflow

--- src/types/vue-shims.d.ts
+++ src/types/vue-shims.d.ts
@@ -3,3 +3,18 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
